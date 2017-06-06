fce314066441b0cc3813e0d7523e35072eebb445
Kamil Postrożny <kamilp009@gmail.com>
2017-06-06 14:01:00 +0200

    Fixed finding oldest people

--- hr/hr.py
+++ hr/hr.py
@@ -136,8 +136,7 @@ def get_oldest_person(table):
 
     sorted_years = common.bubble_sort([item[2] for item in table])
     oldest_people = [item[1] for item in table if item[2] == sorted_years[0]]
-    print(oldest_people)
-
+    return oldest_people
 
 
 # the question: Who is the closest to the average age ?
